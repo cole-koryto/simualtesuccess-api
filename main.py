9d601f9e6634092fff3f42ff3dd6420cfc9a6e6f
colek <colekoryto17@gmail.com>
2024-07-19 17:58:57 -0400

    Hosting changes

--- main.py
+++ main.py
@@ -180,5 +180,4 @@ def main(simulation_inputs: SimulationInputPayload):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, ssl_keyfile="path/to/key.pem", ssl_certfile="path/to/cert.pem")
-#https://plusminuschirag.medium.com/deploying-apis-with-custom-domains-with-nginx-lets-encrypt-fastapi-and-uvicorn-619438168143
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, ssl_keyfile="/etc/letsencrypt/live/simulatesuccess.info/fullchain.pem", ssl_certfile="/etc/letsencrypt/live/simulatesuccess.info/fullchain.pem")
