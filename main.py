b7514c892a6f01bc2c46b8bb5d9c3d39a7aaa7da
colek <colekoryto17@gmail.com>
2024-07-19 18:14:19 -0400

    Hosting changes

--- main.py
+++ main.py
@@ -180,4 +180,4 @@ def main(simulation_inputs: SimulationInputPayload):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, ssl_keyfile="/etc/letsencrypt/live/simulatesuccess.info/fullchain.pem", ssl_certfile="/etc/letsencrypt/live/simulatesuccess.info/fullchain.pem")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, ssl_keyfile="/etc/letsencrypt/live/simulatesuccess.info/privkey.pem", ssl_certfile="/etc/letsencrypt/live/simulatesuccess.info/fullchain.pem")
